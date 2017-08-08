8b8fda1b605701c0774dcf9bd9b6a2ed50b8e2ca
billhu422 <billhu422@126.com>
2017-08-08 10:38:42 +0800

    update: 优化inventory的查询结果

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,31 +11,13 @@ const  rp = require('request-promise');
 
 
 router.get('/v1/hybrid/instance',async(ctx, next)=>{
-
    try{
-        //if(userId == undefined) ctx.throw(400,'{"code"=-4,"description"="Need userId."}');
-       //console.log(ctx.query);
        var option = {
             url:     config.dbRest.baseUrl + '/inventory/instance?'+ querystring.stringify(ctx.query),
             }
-       //console.log(option);
         var dbbody = await rp.get(option);
-        //console.log(dbbody);
-        var instances=[]
-        JSON.parse(dbbody).forEach(function(el){
-            var item = {
-                "orderId": el.orderId,
-                "orderItemId": el.orderItemId,
-                "userId": el.userId,
-                "provider":el.provider,
-                "productName":el.productName,
-                "instanceId":el.instanceId,
-                "region":el.region
-            }
-            instances.push(item);
-        });
         ctx.status = 200;
-        ctx.body ={code:0,instances:instances};
+        ctx.body ={code:0,instances:JSON.parse(dbbody)};
    }
    catch (ex){
        console.log(ex.message);
